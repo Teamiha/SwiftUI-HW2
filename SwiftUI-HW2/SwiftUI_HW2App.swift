f47b84999ce282ee2b199e5af494766956c5979a
Mikhail <jegnum@gmail.com>
2022-05-16 00:53:41 +0300

    Initial Commit

---
+++ SwiftUI-HW2/SwiftUI_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HW2App.swift
+//  SwiftUI-HW2
+//
+//  Created by Михаил Светлов on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
